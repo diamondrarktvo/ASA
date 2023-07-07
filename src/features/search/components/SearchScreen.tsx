c2bb1e1744f7b4dc246004ee0a0fd447350386e1
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-07 12:51:12 +0300

    feat change label in button search

--- src/features/search/components/SearchScreen.tsx
+++ src/features/search/components/SearchScreen.tsx
@@ -3,7 +3,7 @@ import { Button, MainScreen, Text } from "_shared";
 export default function SearchScreen() {
   return (
     <MainScreen typeOfScreen="tab">
-      <Button label="Search" />
+      <Button label="Rechercher" />
     </MainScreen>
   );
 }
