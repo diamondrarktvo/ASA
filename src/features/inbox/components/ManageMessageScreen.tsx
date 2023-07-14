ae735b19c164b634e349a67789ecddcd26fb3d0c
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-14 12:41:56 +0300

    preparation message screen

--- src/features/inbox/components/ManageMessageScreen.tsx
+++ src/features/inbox/components/ManageMessageScreen.tsx
@@ -1,15 +1,7 @@
 import { StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import {
-  Icon,
-  MainScreen,
-  Text,
-  Box,
-  Row,
-  Image,
-  Column,
-  HeaderStackNavNormal,
-} from "_shared";
+import { useRoute, RouteProp } from "@react-navigation/native";
+import { MainScreen, Text, Box, HeaderStackNavNormal } from "_shared";
+import { StackParamList } from "src/navigations/Types";
 import { useTheme } from "@shopify/restyle";
 import { Size, Theme } from "_theme";
 
@@ -17,16 +9,22 @@ export default function ManageMessageScreen() {
   //const navigation = useNavigation<>();
   const theme = useTheme<Theme>();
   const { borderRadii, colors } = theme;
+  const { emetteur } =
+    useRoute<RouteProp<StackParamList, "manage_message">>()?.params;
 
   return (
     <MainScreen typeOfScreen="stack">
       <HeaderStackNavNormal
-        title="Rakoto"
+        title={emetteur}
         subTitle="Délai de réponse : 5 heures"
         iconRight="more-vert"
         iconRightOnPress={() => console.log("test option header")}
       />
-      <Text>All message here</Text>
+      <Box flex={1} justifyContent={"center"} alignItems="center">
+        <Text variant="primary">
+          Vos messages avec {emetteur} apparaissent ici.
+        </Text>
+      </Box>
     </MainScreen>
   );
 }
