f64d52085f7bc81859bd42d23ef6c42cb0699aec
diamondrarktvo <amada.rktvo@gmail.com>
2023-07-27 20:36:34 +0300

    feat:  step 5 done

--- src/features/publish/components/addPublishStep/stepSix.tsx
+++ src/features/publish/components/addPublishStep/stepSix.tsx
@@ -4,11 +4,14 @@ import { Box, Button, Icon, Input, MainScreen, Row, Text } from "_shared";
 import { Size, Theme } from "_theme";
 import { useTheme } from "@shopify/restyle";
 import { stepper7NavigationTypes } from "../../types";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { useGetLocation } from "_hooks";
 
 export default function StepSix() {
   const navigation = useNavigation<stepper7NavigationTypes>();
   const theme = useTheme<Theme>();
   const { borderRadii, colors } = theme;
+  const { position, errorMsgLocation } = useGetLocation();
 
   return (
     <MainScreen typeOfScreen="tab" titleTabScreen="Publication">
@@ -22,22 +25,43 @@ export default function StepSix() {
           Step 6:
         </Text>
         <Text variant={"title"} color="black">
-          Quel produit voulez-vous publier ?
+          Où les clients peuvent-ils vous trouver?
         </Text>
         <Text variant={"tertiary"} color={"error"}>
-          NB: Veuillez remplir chaque étape afin de procéder à votre
-          publication!
+          NB: Vous pouvez entrer dans la barre de recherche le nom de votre
+          ville
         </Text>
         <Box marginVertical={"xs"}>
           <Input
-            placeholder="Nom"
-            value="Kapa"
+            placeholder="ex: Antananarivo"
+            value="Antananarivo"
             iconLeft={{
-              name: "info",
+              name: "place",
               size: Size.ICON_MEDIUM,
               color: colors.text,
             }}
           />
+        </Box>
+        <Box height={250} width={"100%"} marginBottom={"s"}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={[StyleSheet.absoluteFillObject]}
+            initialRegion={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+              latitudeDelta: 0.0222,
+              longitudeDelta: 0.0021,
+            }}
+            showsUserLocation={true}
+            userLocationAnnotationTitle="Vous êtes ici"
+            followsUserLocation={true}
+          >
+            <Marker
+              key={"Vous êtes ici	"}
+              coordinate={position}
+              title={"Vous êtes ici	"}
+            />
+          </MapView>
         </Box>
         <Row alignItems={"center"} justifyContent="space-around">
           <Button
