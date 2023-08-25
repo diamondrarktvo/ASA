0051c8f403727217e70886a6fb47d4eed64b9eef
diamondrarktvo <amada.rktvo@gmail.com>
2023-08-25 16:29:12 +0300

    feat: custom screen register

--- src/features/account/components/CreateAccountScreen.tsx
+++ src/features/account/components/CreateAccountScreen.tsx
@@ -14,59 +14,75 @@ import { useTheme } from "@shopify/restyle";
 import { Theme, Size } from "_theme";
 import { ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { RadioButton } from "react-native-paper";
 
 const CreateAccountScreen = () => {
   const [hidePassword, setHidePassword] = useState(false);
-  const [hideConfirmPassword, setHideConfirmPassword] = useState(false);
+  const [hideConfirmPassword, setHideConfirmPassword] = useState(true);
+  const [isThePasswordSame, setIsThePasswordSame] = useState(false);
+  const [checked, setChecked] = useState("yes");
   const navigation = useNavigation();
   const theme = useTheme<Theme>();
-  const { primary, secondary, white } = theme.colors;
+  const { colors } = theme;
 
   return (
     <MainScreen typeOfScreen="stack">
       <ScrollView showsVerticalScrollIndicator={false}>
         <Box paddingVertical="m" backgroundColor="mainBackground">
-          <Box>
-            <Text variant="headerNavigation" color="text">
-              Bienvenue
-            </Text>
-            <Row marginBottom="s">
-              <Text variant="title" color="secondary">
-                Remplissez les informations suivantes afin de creer votre compte
-                sur notre application Mety Amiko.
-              </Text>
+          <Text variant="headerNavigation" color="text">
+            Bienvenue
+          </Text>
+          <Column>
+            <Input placeholder="Nom*" />
+            <Input placeholder="Prénom*" />
+            <Input placeholder="Pseudo*" />
+            <Input placeholder="Age*" />
+            <Input placeholder="Numéro téléphone*" />
+            <Input placeholder="Email" />
+            <Row justifyContent="space-around" alignItems={"center"}>
+              <Text variant={"secondary"}>Vous êtes professionel ? </Text>
+              <RadioButton
+                value="first"
+                color={colors.primary}
+                status={checked === "yes" ? "checked" : "unchecked"}
+                onPress={() => setChecked("yes")}
+              />
+              <Text variant="tertiary">Oui</Text>
+              <RadioButton
+                value="second"
+                color={colors.primary}
+                status={checked === "no" ? "checked" : "unchecked"}
+                onPress={() => setChecked("no")}
+              />
+              <Text variant="tertiary">Non</Text>
             </Row>
-            <Column>
-              <Input placeholder="Prénom usuel*" />
-              <Input placeholder="Numéro téléphone*" />
-              <Input placeholder="Adresse*" />
-              <Input placeholder="Type de compte (à choisir)*" />
-              <Input placeholder="Email*" />
-              <Input
-                placeholder="Mot de passe*"
-                iconRight={{
-                  name: hidePassword ? "visibility" : "visibility-off",
-                  size: 32,
-                  color: secondary,
-                  onPress: () => setHidePassword(!hidePassword),
-                }}
-              />
-              <Input
-                placeholder="Confirmer mot de passe*"
-                iconRight={{
-                  name: hideConfirmPassword ? "visibility" : "visibility-off",
-                  size: 32,
-                  color: secondary,
-                  onPress: () => setHideConfirmPassword(!hideConfirmPassword),
-                }}
-              />
-            </Column>
-            <Button
-              variant={"primary"}
-              label="Creer mon compte"
-              onPress={() => navigation.goBack()}
+            <Input
+              placeholder="Mot de passe*"
+              secureTextEntry={hideConfirmPassword}
+              iconRight={{
+                name: hidePassword ? "visibility" : "visibility-off",
+                size: 32,
+                color: colors.secondary,
+                onPress: () => setHidePassword(!hidePassword),
+              }}
             />
-          </Box>
+            <Input
+              placeholder="Confirmer mot de passe*"
+              secureTextEntry={hideConfirmPassword}
+              iconRight={{
+                name: hideConfirmPassword ? "visibility" : "visibility-off",
+                size: 32,
+                color: colors.secondary,
+                onPress: () => setHideConfirmPassword(!hideConfirmPassword),
+              }}
+            />
+          </Column>
+          <Button
+            variant={"primary"}
+            label="Creer mon compte"
+            onPress={() => navigation.goBack()}
+            marginTop={"s"}
+          />
           <Row
             alignItems="center"
             padding="s"
@@ -76,7 +92,11 @@ const CreateAccountScreen = () => {
             marginTop="xs"
           >
             <TouchableOpacity onPress={() => navigation.goBack()}>
-              <Icon name="arrow-back" size={Size.ICON_LARGE} color={primary} />
+              <Icon
+                name="arrow-back"
+                size={Size.ICON_LARGE}
+                color={colors.primary}
+              />
             </TouchableOpacity>
             <Text variant="primaryBold" color="text">
               Se connecter
