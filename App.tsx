1e1d6f44b6916a4df51b78549b04043bb05bddc5
diamondrarktvo <amada.rktvo@gmail.com>
2023-09-27 00:15:22 +0300

    feat: move socket in navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,4 @@
 import "react-native-gesture-handler";
-navigator.__defineGetter__("userAgent", function () {
-  // you have to import rect native first !!
-  return "react-native";
-});
 import { useEffect, useState } from "react";
 import { ThemeProvider, useTheme } from "@shopify/restyle";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -13,38 +9,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { Provider } from "react-redux";
 import { store } from "_store";
-import io from "socket.io-client";
-
-const SOCKET_URL =
-  "wss://metyamikoservice-api.onrender.com/ws/notification/joe";
 
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [connected, setConnected] = useState(false);
-
-  const socket = io(SOCKET_URL, {
-    transports: ["websocket"],
-    auth: {
-      access_token: "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d",
-    },
-  });
-
-  useEffect(() => {
-    console.log("depart e");
-    console.log("socket : ", socket);
-    socket.on("connected", () => {
-      console.log("Connected to server e");
-    });
-
-    /*socket.on("connect_error", (err) => {
-      console.log(err);
-    });*/
-
-    socket.on("disconnect", () => {
-      console.log("Disconnected from server");
-    });
-    console.log("fin");
-  }, []);
 
   return (
     <Provider store={store}>
--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -9,6 +9,8 @@ import { StackParamList } from "./Types";
 //IMPORT NAVIGATION TAB
 import TabNavigation from "./TabNavigation";
 
+import io from "socket.io-client";
+
 //IMPORT SCREEN
 import {
   ManageProfil,
@@ -25,10 +27,68 @@ import {
   ManagePayment,
   ProductDetailScreen,
 } from "_features";
+import { useAppSelector } from "_store";
+import { useEffect, useState } from "react";
 
 const Stack = createStackNavigator<StackParamList>();
 
+const SOCKET_URL = process.env.EXPO_PUBLIC_API_HOST || "";
+
 const StackNavigation = () => {
+  const accountUser = useAppSelector((state) => state.account);
+
+  console.log("accountUser : ", accountUser);
+  console.log("SOCKET_URL : ", SOCKET_URL);
+
+  const socket = io(SOCKET_URL, {
+    transports: ["websocket"],
+    path: "/ws/notification/Vetsokeli",
+    query: {
+      access_token: "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d",
+    },
+    auth: {
+      access_token: "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d",
+    },
+    extraHeaders: {
+      access_token: "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d",
+    },
+  });
+
+  /*const socket = new WebSocket(
+    "https://metyamikoservice-api.onrender.com/ws/notification/Vetsokeli",
+    ["access_token", "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d"],
+  );*/
+
+  useEffect(() => {
+    console.log("depart e");
+    console.log("chatSocket : ", socket);
+    socket.on("connect", () => {
+      console.log("Connected to server e");
+    });
+
+    socket.on("open", () => {
+      console.log("Connected to server e");
+    });
+
+    // verify connection
+    /*socket.onopen = function (e) {
+      console.log("The connection was setup successfully !");
+    };*/
+    // verify error
+    /*socket.onclose = function (e) {
+      console.log("Something unexpected happened !");
+    };*/
+
+    /*socket.on("connect_error", (err) => {
+      console.log(err);
+    });*/
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+    console.log("fin");
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={"main_tab"}>
