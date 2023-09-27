d036a2caf56b93a4d9f2f74ef0f33ea196cd06f0
diamondrarktvo <amada.rktvo@gmail.com>
2023-09-27 11:34:26 +0300

    feat: remove socket.io

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import "react-native-gesture-handler";
-import { useEffect, useState } from "react";
-import { ThemeProvider, useTheme } from "@shopify/restyle";
+import { useState } from "react";
+import { ThemeProvider } from "@shopify/restyle";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StackNavigation } from "_navigations";
 import { theme, darkTheme } from "_theme";
@@ -9,32 +9,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { Provider } from "react-redux";
 import { store } from "_store";
-import io from "socket.io-client";
-
-const SOCKET_URL =
-  "wss://metyamikoservice-api.onrender.com/ws/notificaiton/joe";
 
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [connected, setConnected] = useState(false);
-
-  const socket = io(SOCKET_URL, {
-    query: {
-      access_token: "86567dd03ac2be30f1d58ebd811bcd2a4ed1d72d",
-    },
-  });
-
-  useEffect(() => {
-    console.log("depart e");
-    socket.on("connect", () => {
-      console.log("Connected to server e");
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Disconnected from server");
-    });
-    console.log("fin");
-  });
 
   return (
     <Provider store={store}>
