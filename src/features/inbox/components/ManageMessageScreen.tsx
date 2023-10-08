d836a92bb68c6305bcd98db9be087acc6b534512
diamondrarktvo <amada.rktvo@gmail.com>
2023-10-08 19:23:45 +0300

    feat: manage message screen

--- src/features/inbox/components/ManageMessageScreen.tsx
+++ src/features/inbox/components/ManageMessageScreen.tsx
@@ -11,6 +11,7 @@ import {
   HeaderStackNavNormal,
   RequestLoader,
   RequestError,
+  Button,
 } from "_shared";
 import { StackParamList } from "src/navigations/Types";
 import { useTheme } from "@shopify/restyle";
@@ -21,6 +22,7 @@ import { useAppDispatch, useAppSelector } from "_store";
 import { useGetMessageInConversationQuery } from "../chatApi";
 import { removeAccount } from "../../account/accountSlice";
 import { GiftedChat, Send } from "react-native-gifted-chat";
+import AnimatedLottieView from "lottie-react-native";
 
 export default function ManageMessageScreen() {
   //const navigation = useNavigation<>();
@@ -29,6 +31,7 @@ export default function ManageMessageScreen() {
   const dispatch = useAppDispatch();
   const accountUser = useAppSelector((state) => state.account);
   const [messages, setMessages] = useState<any[]>([]);
+  const [isMessageAlreadyStart, setIsMessageAlreadyStart] = useState(false);
   const { emetteur } =
     useRoute<RouteProp<StackParamList, "manage_message">>()?.params;
   const {
@@ -121,33 +124,62 @@ export default function ManageMessageScreen() {
           iconRight="more-vert"
           iconRightOnPress={() => openBottomSheet()}
         />
-        <Box flex={1}>
-          <GiftedChat
-            messages={messages}
-            placeholder="Ecrivez un message ici..."
-            onSend={(messages) => onSend(messages)}
-            user={{
-              _id: 1,
-            }}
-            isLoadingEarlier={isMessageLoading || isMessageFetching}
-            renderLoading={() => (
-              <Box flex={1} justifyContent={"center"} alignItems="center">
-                <Text variant="primary">Chargement...</Text>
-              </Box>
-            )}
-            renderSend={(props) => (
-              <Send {...props}>
-                <Box mb={"m"} mr={"s"}>
-                  <Icon
-                    name="send"
-                    size={Size.ICON_LARGE}
-                    color={colors.primary}
-                  />
+        {isMessageAlreadyStart ? (
+          <Box flex={1}>
+            <GiftedChat
+              messages={messages}
+              placeholder="Ecrivez un message ici..."
+              onSend={(messages) => onSend(messages)}
+              user={{
+                _id: 1,
+              }}
+              isLoadingEarlier={isMessageLoading || isMessageFetching}
+              renderLoading={() => (
+                <Box flex={1} justifyContent={"center"} alignItems="center">
+                  <Text variant="primary">Chargement...</Text>
                 </Box>
-              </Send>
-            )}
-          />
-        </Box>
+              )}
+              renderSend={(props) => (
+                <Send {...props}>
+                  <Box mb={"m"} mr={"s"}>
+                    <Icon
+                      name="send"
+                      size={Size.ICON_LARGE}
+                      color={colors.primary}
+                    />
+                  </Box>
+                </Send>
+              )}
+            />
+          </Box>
+        ) : (
+          <Box flex={1} justifyContent={"center"} alignItems={"center"}>
+            <AnimatedLottieView
+              source={require("_images/conversation.json")}
+              autoPlay
+              loop
+              style={styles.lottieImg}
+            />
+            <Text variant="primary" textAlign="center">
+              Cliquez sur la flèche pour démarrer votre conversation
+            </Text>
+            <Icon
+              name="arrow-forward"
+              size={Size.ICON_LARGE}
+              color={colors.white}
+              style={{
+                marginTop: 8,
+                backgroundColor: colors.blue,
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                textAlign: "center",
+                padding: 8,
+                color: colors.white,
+              }}
+            />
+          </Box>
+        )}
 
         <BottomSheetModal
           backdropComponent={renderBackDrop}
@@ -187,5 +219,8 @@ export default function ManageMessageScreen() {
 const styles = StyleSheet.create({
   bottomSheet_container: {
     paddingHorizontal: "4%",
+  },
+  lottieImg: {
+    height: 250,
   },
 });
