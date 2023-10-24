cdb6565a5a2b684c501ca74075e9e4da8ce8f2bb
diamondrarktvo <amada.rktvo@gmail.com>
2023-10-24 11:56:15 +0300

    feat: dynamise publication data

--- src/features/publish/components/addPublishStep/stepSeven.tsx
+++ src/features/publish/components/addPublishStep/stepSeven.tsx
@@ -1,121 +1,264 @@
 import { StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Box, Button, Icon, Input, MainScreen, Row, Text } from "_shared";
+import {
+  Box,
+  Button,
+  CheckUserConnected,
+  Input,
+  MainScreen,
+  Row,
+  Text,
+} from "_shared";
 import { Size, Theme } from "_theme";
 import { useTheme } from "@shopify/restyle";
-import { stepper3NavigationTypes } from "../../types";
-import { CheckBox } from "@rneui/themed";
+import { RadioButton } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "_store";
+import { reinitializeProduct, selectors } from "../../publishSlice";
 
 export default function StepSeven() {
   const navigation = useNavigation();
+  const dispatch = useAppDispatch();
   const theme = useTheme<Theme>();
   const { borderRadii, colors } = theme;
+  const currentProduct = useAppSelector(selectors.selectProductToPublish);
+  const accountUser = useAppSelector((state) => state.account.user);
+  const [valueForStepper, setValueForStepper] = useState(currentProduct);
+  const isUserConnected = useAppSelector(
+    (state) => state.account.is_account_connected,
+  );
+  const [userMustLogin, setUserMustLogin] = useState<boolean>(isUserConnected);
+  const [valueOfRadioButton, setValueOfRadioButton] = useState({
+    showNumber: { label: "Oui", value: "yes" },
+    showMail: { label: "Oui", value: "yes" },
+    acceptSalesApproach: { label: "Oui", value: "yes" },
+  });
+  const [contactSeller, setContactSeller] = useState<{
+    phone_number: string;
+    email: string;
+  }>({
+    phone_number: accountUser.phone_number ?? "",
+    email: accountUser.email ?? "",
+  });
+
+  const handlePublish = () => {
+    if (isUserConnected) {
+      //console.log("valueForStepper step before dispatch : ", valueForStepper);
+      dispatch(reinitializeProduct());
+      navigation.navigate("main_tab", { screen: "publish_screen" });
+    } else {
+      setUserMustLogin(!isUserConnected);
+    }
+  };
+
+  //all effects
+  useEffect(() => {
+    setContactSeller({
+      phone_number: accountUser.phone_number,
+      email: accountUser.email,
+    });
+    setValueForStepper((prevState) => ({
+      ...prevState,
+      email_contact: accountUser.email,
+      phone_number_contact: accountUser.phone_number,
+    }));
+  }, [accountUser]);
 
   return (
     <MainScreen typeOfScreen="tab" titleTabScreen="Publication">
-      <Box marginTop={"m"}>
-        <Text
-          variant={"primary"}
-          color={"blue"}
-          textDecorationLine={"underline"}
-          marginBottom={"xs"}
-        >
-          Etape finale:
-        </Text>
-        <Text variant={"title"} color="black">
-          Configuration de votre contact
-        </Text>
-        <Text variant={"tertiary"} color={"error"}>
-          NB: Vous pouvez ici afficher votre numéro, mail en cochant les cases
-        </Text>
-        <Box marginVertical={"xs"}>
-          <Input
-            placeholder="Votre numéro téléphone"
-            value="+261 34 56 484 25"
-            iconLeft={{
-              name: "call",
-              size: Size.ICON_MEDIUM,
-              color: colors.text,
-            }}
-          />
-          <Row justifyContent="space-around" alignItems={"center"}>
-            <Text variant={"secondary"}>Afficher le numéro : </Text>
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={false}
-              title="Oui"
-            />
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={true}
-              title="Non"
-            />
-          </Row>
-          <Input
-            placeholder="Votre adresse email"
-            value="metyamiko@gmail.com"
-            iconLeft={{
-              name: "mail",
-              size: Size.ICON_MEDIUM,
-              color: colors.text,
-            }}
-          />
-          <Row justifyContent="space-around" alignItems={"center"}>
-            <Text variant={"secondary"}>Afficher le mail : </Text>
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={false}
-              title="Oui"
-            />
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={true}
-              title="Non"
-            />
-          </Row>
-          <Text variant={"primaryBold"}>
-            * Accepter le démarche commercial ?{" "}
+      <CheckUserConnected
+        userMustLogin={userMustLogin}
+        setUserMustLogin={setUserMustLogin}
+        subTitleIfNotConnected="Connectez-vous pour découvrir toutes nos fonctionnalités"
+      >
+        <Box marginTop={"m"}>
+          <Text
+            variant={"primary"}
+            color={"blue"}
+            textDecorationLine={"underline"}
+            marginBottom={"xs"}
+          >
+            Etape finale:
           </Text>
-          <Row justifyContent="space-around" alignItems={"center"}>
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={true}
-              title="Oui"
+          <Text variant={"title"} color="black">
+            Configuration de votre contact
+          </Text>
+          <Text variant={"tertiary"} color={"error"}>
+            NB: Vous pouvez ici afficher votre numéro, mail en cochant les cases
+          </Text>
+          <Box marginVertical={"xs"}>
+            <Input
+              placeholder="Votre numéro téléphone"
+              value={contactSeller.phone_number}
+              iconLeft={{
+                name: "call",
+                size: Size.ICON_MEDIUM,
+                color: colors.text,
+              }}
             />
-            <CheckBox
-              containerStyle={{ backgroundColor: colors.mainBackground }}
-              checkedColor={colors.primary}
-              checked={false}
-              title="Non"
+            <Row justifyContent="space-around" alignItems={"center"}>
+              <Text variant={"secondary"}>Afficher le numéro : </Text>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"yes"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.showNumber.value === "yes"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        showNumber: { label: "Oui", value: "yes" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Oui</Text>
+              </Box>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"no"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.showNumber.value === "no"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        showNumber: { label: "No", value: "no" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Non</Text>
+              </Box>
+            </Row>
+            <Input
+              placeholder="Votre adresse email"
+              value={contactSeller.email}
+              iconLeft={{
+                name: "mail",
+                size: Size.ICON_MEDIUM,
+                color: colors.text,
+              }}
+            />
+            <Row justifyContent="space-around" alignItems={"center"}>
+              <Text variant={"secondary"}>Afficher le mail : </Text>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"yes"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.showMail.value === "yes"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        showMail: { label: "Oui", value: "yes" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Oui</Text>
+              </Box>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"no"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.showMail.value === "no"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        showMail: { label: "No", value: "no" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Non</Text>
+              </Box>
+            </Row>
+            <Text variant={"primaryBold"}>
+              * Accepter le démarche commercial ?{" "}
+            </Text>
+            <Row justifyContent="space-around" alignItems={"center"}>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"yes"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.acceptSalesApproach.value === "yes"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        acceptSalesApproach: { label: "Oui", value: "yes" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Oui</Text>
+              </Box>
+              <Box flexDirection={"row"} alignItems={"center"}>
+                <RadioButton
+                  value={"no"}
+                  color={colors.primary}
+                  status={
+                    valueOfRadioButton.acceptSalesApproach.value === "no"
+                      ? "checked"
+                      : "unchecked"
+                  }
+                  onPress={() =>
+                    setValueOfRadioButton((prevState) => {
+                      return {
+                        ...prevState,
+                        acceptSalesApproach: { label: "No", value: "no" },
+                      };
+                    })
+                  }
+                />
+                <Text variant="tertiary">Non</Text>
+              </Box>
+            </Row>
+          </Box>
+          <Row alignItems={"center"} justifyContent="space-around">
+            <Button
+              height={50}
+              alignItems={"center"}
+              justifyContent={"center"}
+              width={150}
+              variant={"tertiary"}
+              label="Précédent"
+              onPress={() => navigation.goBack()}
+            />
+            <Button
+              height={50}
+              alignItems={"center"}
+              justifyContent={"center"}
+              width={150}
+              variant={"primary"}
+              label="Publier"
+              onPress={() => handlePublish()}
             />
           </Row>
         </Box>
-        <Row alignItems={"center"} justifyContent="space-around">
-          <Button
-            height={50}
-            alignItems={"center"}
-            justifyContent={"center"}
-            width={150}
-            variant={"tertiary"}
-            label="Précédent"
-            onPress={() => navigation.goBack()}
-          />
-          <Button
-            height={50}
-            alignItems={"center"}
-            justifyContent={"center"}
-            width={150}
-            variant={"primary"}
-            label="Publier"
-            onPress={() => console.log("done")}
-          />
-        </Row>
-      </Box>
+      </CheckUserConnected>
     </MainScreen>
   );
 }
