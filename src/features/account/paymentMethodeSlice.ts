1686dc9a6ba423e93eb68ba9a47e7c875d2ca4e9
diamondrarktvo <amada.rktvo@gmail.com>
2023-11-01 21:29:23 +0300

    feat: list number payment in managePayment

--- src/features/account/paymentMethodeSlice.ts
+++ src/features/account/paymentMethodeSlice.ts
@@ -5,11 +5,13 @@ import { removeDataToAsyncStorage } from "_utils";
 export interface paymentMethodState {
   id: number | null;
   user: string | null;
-  name: string | null;
+  name_mobile_money: string | null;
+  type: string;
   phone: string | null;
   card_number: string | null;
   expiration_date: string | null;
   cvv: string | null;
+  paypal_email: string | null;
 }
 
 const initialState: paymentMethodState[] = [];
